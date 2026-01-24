e84b73d88f8165f007e2030f58878bbd789d9f3e
Code4Data24 <rinkuguptayt457@gmail.com>
2026-01-24 15:26:37 +0530

    Add files via upload

---
+++ types/index.d.ts
@@ -0,0 +1,54 @@
+
+
+declare type FileType = "document" | "image" | "video" | "audio" | "other";
+
+declare interface ActionType {
+  label: string;
+  icon: string;
+  value: string;
+}
+
+declare interface SearchParamProps {
+  params?: Promise<SegmentParams>;
+  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+declare interface GetFilesProps {
+  types: FileType[];
+  searchText?: string;
+  sort?: string;
+  limit?: number;
+}
+declare interface RenameFileProps {
+  fileId: string;
+  name: string;
+  extension: string;
+  path: string;
+}
+
+declare interface MobileNavigationProps {
+  userId: string;
+  fullName: string;
+  avatar: string;
+  email: string;
+}
+
+declare interface FileUploaderProps {
+  userId: string;
+  className?: string;
+} 
+
+declare interface SidebarProps {
+  fullName: string;
+  avatar: string;
+  email: string;
+}
+
+declare interface ThumbnailProps {
+  type: string;
+  extension: string;
+  url: string;
+  className?: string;
+  imageClassName?: string;
+}
+
